2d1b596ea146b0a0746ac1ccdaa378710aa02ad7
Thais Tamaio <77994638+ThaisTamaio@users.noreply.github.com>
2022-04-20 21:48:22 -0500

    Taller 2 commit final?

--- main.ts
+++ main.ts
@@ -1,30 +1,81 @@
 import { Serie } from './serie.js';
-
 import { series } from './data.js';
 
+
+//Variables obtenidas de data.ts:
+
 let seriesTbody: HTMLElement = document.getElementById('series')!;
+let card: HTMLElement = document.getElementById('card')!;
+let card_image: HTMLImageElement = document.getElementById('imagen-serie')! as HTMLImageElement;
+let card_tittle: HTMLElement = document.getElementById('titulo-serie')!;
+let card_text: HTMLElement = document.getElementById('descripcion-serie')!;
+let card_link: HTMLAnchorElement = document.getElementById('link-serie')! as HTMLAnchorElement;
+
+
+//Variables calculadas:
+
 const avgTemporadas: HTMLElement = document.getElementById("avg-temporadas")!;
 
 renderSeriesInTable(series);
 avgTemporadas.innerHTML = `${getPromedioTemporadas(series)}`
 
 
+//Funciones:
+
 function renderSeriesInTable(series: Serie[]): void {
   console.log('Desplegando series');
   series.forEach((serie) => {
+
     let trElement = document.createElement("tr");
-    trElement.innerHTML = `<td class="bold-font">${serie.posicion}</td>
-                           <td class="color-blue"><input type = "button" value = "${serie.titulo}" class="btn btn-link series-button"></button></td>
-                           <td>${serie.canal}</td>
-                           <td>${serie.temporadas}</td>`;
+    let tdElement = document.createElement("td");
+    let spanElement = document.createElement("span");
+    let posicionActual = serie.posicion - 1
+
+    trElement.classList.add('columnas-grises');
+
+    tdElement.innerHTML = `${serie.posicion}`;
+    trElement.appendChild(tdElement);
+
+    tdElement = document.createElement("td");
+    spanElement.innerHTML = `${serie.titulo}`;
+    spanElement.classList.add('boton');
+    spanElement.onclick = () => seleccionSerie(posicionActual);
+    tdElement.appendChild(spanElement);
+    trElement.appendChild(tdElement);
+
+    tdElement = document.createElement("td");
+    tdElement.innerHTML = `${serie.canal}`;
+    trElement.appendChild(tdElement);
+
+    tdElement = document.createElement("td");
+    tdElement.innerHTML = `${serie.temporadas}`;
+    trElement.appendChild(tdElement);
     seriesTbody.appendChild(trElement);
   });
 }
 
+
+//Función que calcula el promedio de temporadas:
+
 function getPromedioTemporadas(series: Serie[]): number {
   let promedioTemporadas: number = 0;
   series.forEach((serie) => promedioTemporadas = promedioTemporadas + serie.temporadas);
-  return promedioTemporadas / series.length;
+  let promedio: number = promedioTemporadas / series.length;
+  return promedio;
+}
+
+
+//"Event Listener":
+
+function seleccionSerie(infoSeries: any) {
+  let serie = series[infoSeries];
+
+  card.style.display = "block";
+  card_image.src = serie.imagen;
+  card_tittle.innerHTML = serie.titulo;
+  card_text.innerHTML = serie.descripcion;
+  card_link.innerHTML = serie.link;
+  card_link.href = serie.link;
 }
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,55 @@
 import { series } from './data.js';
+//Variables obtenidas de data.ts:
 var seriesTbody = document.getElementById('series');
+var card = document.getElementById('card');
+var card_image = document.getElementById('imagen-serie');
+var card_tittle = document.getElementById('titulo-serie');
+var card_text = document.getElementById('descripcion-serie');
+var card_link = document.getElementById('link-serie');
+//Variables calculadas:
 var avgTemporadas = document.getElementById("avg-temporadas");
 renderSeriesInTable(series);
 avgTemporadas.innerHTML = "".concat(getPromedioTemporadas(series));
+//Funciones:
 function renderSeriesInTable(series) {
     console.log('Desplegando series');
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td class=\"bold-font\">".concat(serie.posicion, "</td>\n                           <td class=\"color-blue\"><input type = \"button\" value = \"").concat(serie.titulo, "\" class=\"btn btn-link series-button\"></button></td>\n                           <td>").concat(serie.canal, "</td>\n                           <td>").concat(serie.temporadas, "</td>");
+        var tdElement = document.createElement("td");
+        var spanElement = document.createElement("span");
+        var posicionActual = serie.posicion - 1;
+        trElement.classList.add('columnas-grises');
+        tdElement.innerHTML = "".concat(serie.posicion);
+        trElement.appendChild(tdElement);
+        tdElement = document.createElement("td");
+        spanElement.innerHTML = "".concat(serie.titulo);
+        spanElement.classList.add('boton');
+        spanElement.onclick = function () { return seleccionSerie(posicionActual); };
+        tdElement.appendChild(spanElement);
+        trElement.appendChild(tdElement);
+        tdElement = document.createElement("td");
+        tdElement.innerHTML = "".concat(serie.canal);
+        trElement.appendChild(tdElement);
+        tdElement = document.createElement("td");
+        tdElement.innerHTML = "".concat(serie.temporadas);
+        trElement.appendChild(tdElement);
         seriesTbody.appendChild(trElement);
     });
 }
+//Función que calcula el promedio de temporadas:
 function getPromedioTemporadas(series) {
     var promedioTemporadas = 0;
     series.forEach(function (serie) { return promedioTemporadas = promedioTemporadas + serie.temporadas; });
-    return promedioTemporadas / series.length;
+    var promedio = promedioTemporadas / series.length;
+    return promedio;
+}
+//"Event Listener":
+function seleccionSerie(infoSeries) {
+    var serie = series[infoSeries];
+    card.style.display = "block";
+    card_image.src = serie.imagen;
+    card_tittle.innerHTML = serie.titulo;
+    card_text.innerHTML = serie.descripcion;
+    card_link.innerHTML = serie.link;
+    card_link.href = serie.link;
 }
